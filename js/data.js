7e9710fdc4c3e4ae69a864bb90fdbea46c6030d5
Alvaro Bonilla <alvarobonillavillalba@gmail.com>
2019-01-25 12:27:51 +0100

    dash level

--- js/data.js
+++ js/data.js
@@ -215,101 +215,189 @@ var totalGame = [
             y: 500
         }
     },
-
     {
-        level: 22,
+        level: 3,
         platforms: [
             {
-                img: "img/blue.png",
+                img: "img/black.png",
                 x: 0,
-                y: 440,
-                w: 1200,
-                h: 60
-            },
-            {
-                img: "img/blue.png",
-                x: 50,
                 y: 0,
-                w: 60,
+                w: 2,
                 h: 600
             },
             {
-                img: "img/green.png",
-                x: 800,
-                y: 350,
-                w: 200,
-                h: 200,
-                isDashBreakable: true,
-                isDownBreakable: true
-            }
-        
-        ],
-        backImages: [
+                img: "img/transparent.png",
+                x: 0,
+                y: 480,
+                w: 1200,
+                h: 2
+            },
             {
-                x: 300,
+                img: "img/black.png",
+                x: 1200,
                 y: 0,
-                w: 5,
-                h: 600,
-                src: "img/blue.png",
+                w: 2,
+                h: 600
             }
-        ],
-        frontImages: [
+            ,
             {
-                x: 400,
-                y: 0,
-                w: 5,
-                h: 600,
-                src: "img/blue.png",
+                img: "img/break5.png",
+                x: 990,
+                y: 340,
+                w: 70,
+                h: 130,
+                isDashBreakable:true
+
+            },
+            {
+                img: "img/plat1.png",
+                x: 980,
+                y: 300,
+                w: 160,
+                h: 60
+
             }
         ],
         collectableItems: [
             {
-                x: 400,
+                x: 1090,
                 y: 360,
-                w: 60,
-                h: 15,
+                w: 50,
+                h: 50,
                 isActive: true,
-                src: "img/green.png",
-                damage: false
+                src: "img/item.png"
             },
-            {
-                x: 200,
-                y: 360,
-                w: 60,
-                h: 15,
-                isActive: false,
-                src: "img/green.png",
-                damage: false
-            }
         ],
         powerItems: [
             {
-                x: 111,
-                y: 460,
-                w: 15,
-                h: 15,
+                x: 590,
+                y: 390,
+                w: 40,
+                h: 40,
                 isActive: true,
-                src: "img/blue.png",
+                src: "img/power1.png",
                 type:"dash"
             }
         ],
-        damageItems: [
+        background: {
+            src:"img/fondo0_2.png"
+        },
+        frontImages: [
             {
-                x: 600,
+                x: 100,
+                y: 300,
+                w: 150,
+                h: 300,
+                src: "img/stone8.png",
+            },
+            {
+                x: 215,
                 y: 400,
-                w: 60,
-                h: 15,
-                isActive: true,
-                src: "img/red.png",
-                damage: true
+                w: 180,
+                h: 200,
+                src: "img/stone6.png",
             }
         ],
-        background: {
-            src:"img/grey.png"
-        },
         player : {
-            x: 800,
-            y: 200
+            x: 50,
+            y: 350
         }
-    }
+    },
+
+    // {
+    //     level: 22,
+    //     platforms: [
+    //         {
+    //             img: "img/blue.png",
+    //             x: 0,
+    //             y: 440,
+    //             w: 1200,
+    //             h: 60
+    //         },
+    //         {
+    //             img: "img/blue.png",
+    //             x: 50,
+    //             y: 0,
+    //             w: 60,
+    //             h: 600
+    //         },
+    //         {
+    //             img: "img/green.png",
+    //             x: 800,
+    //             y: 350,
+    //             w: 200,
+    //             h: 200,
+    //             isDashBreakable: true,
+    //             isDownBreakable: true
+    //         }
+        
+    //     ],
+    //     backImages: [
+    //         {
+    //             x: 300,
+    //             y: 0,
+    //             w: 5,
+    //             h: 600,
+    //             src: "img/blue.png",
+    //         }
+    //     ],
+    //     frontImages: [
+    //         {
+    //             x: 400,
+    //             y: 0,
+    //             w: 5,
+    //             h: 600,
+    //             src: "img/blue.png",
+    //         }
+    //     ],
+    //     collectableItems: [
+    //         {
+    //             x: 400,
+    //             y: 360,
+    //             w: 60,
+    //             h: 15,
+    //             isActive: true,
+    //             src: "img/green.png",
+    //             damage: false
+    //         },
+    //         {
+    //             x: 200,
+    //             y: 360,
+    //             w: 60,
+    //             h: 15,
+    //             isActive: false,
+    //             src: "img/green.png",
+    //             damage: false
+    //         }
+    //     ],
+    //     powerItems: [
+    //         {
+    //             x: 111,
+    //             y: 460,
+    //             w: 15,
+    //             h: 15,
+    //             isActive: true,
+    //             src: "img/blue.png",
+    //             type:"dash"
+    //         }
+    //     ],
+    //     damageItems: [
+    //         {
+    //             x: 600,
+    //             y: 400,
+    //             w: 60,
+    //             h: 15,
+    //             isActive: true,
+    //             src: "img/red.png",
+    //             damage: true
+    //         }
+    //     ],
+    //     background: {
+    //         src:"img/grey.png"
+    //     },
+    //     player : {
+    //         x: 800,
+    //         y: 200
+    //     }
+    // }
 ]
